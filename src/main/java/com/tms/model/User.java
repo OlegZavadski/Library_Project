1aacfe2ba06356896967b826c4ccbb304b82c7f6
OlegZavadski <z.oleg.f@yandex.ru>
2022-10-02 18:01:50 +0300

    Some changes

--- src/main/java/com/tms/model/User.java
+++ src/main/java/com/tms/model/User.java
@@ -6,7 +6,9 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
