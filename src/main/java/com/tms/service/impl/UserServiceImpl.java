ec1156c30f938c6882ed04811b242651d16a6459
OlegZavadski <z.oleg.f@yandex.ru>
2022-10-16 18:53:02 +0300

    Some changes

--- src/main/java/com/tms/service/impl/UserServiceImpl.java
+++ src/main/java/com/tms/service/impl/UserServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
